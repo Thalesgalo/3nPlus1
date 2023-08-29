d484c0a5474ba3e3d671f1f0c9ccf7857f6a566f
Thales Cancado Costa <Thales.Costa@rhimagnesita.com>
2023-08-29 10:24:36 -0300

    Introdução da biblioteca e inicio do desenvolvimento do algoritmo

--- Thalesgalo/Biblioteca/Biblioteca/Class1.cs
+++ Thalesgalo/Biblioteca/Biblioteca/Class1.cs
@@ -1,7 +1,35 @@
 ﻿namespace Biblioteca
 {
-    public class Class1
+    public class Calculos
     {
+        public int calulos(int n)
+        {
+            int cont = 0;
+
+            if (n == 1)
+            {
+                return 0;
+            }
+
+            while (n == 1)
+            {
+                if (n % 2 == 0)
+                {
+                    cont = 3 * n + 1;
+                }
+                else if (n % 2 - 1 == 0)
+                {
+                    cont = n/2;
+                }
+                
+            }
+
+            return cont;
+
+
+        }
+
+
 
 
     }
--- Thalesgalo/Console/Program.cs
+++ Thalesgalo/Console/Program.cs
@@ -1,12 +1,19 @@
-﻿namespace Console
+﻿using System;
+
+namespace ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            int n;
 
+            Console.Write("Informe o valor do numero para o teste:");
+            n = int.Parse(Console.ReadLine());
 
+            Console.Write(calculos(n));
+
+            Console.ReadKey(true);
         }
     }
 }
