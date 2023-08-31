1f2625a20c50c4c0436dae859ccf016b8f4b953a
Thales Cancado Costa <Thales.Costa@rhimagnesita.com>
2023-08-31 10:54:44 -0300

    Instalação do FluentAssertions e criação do primeiro Teste

--- Thalesgalo/Teste/Teste/UnitTest1.cs
+++ Thalesgalo/Teste/Teste/UnitTest1.cs
@@ -1,12 +1,23 @@
+using Biblioteca;
+using Xunit;
+using FluentAssertions;
+
 namespace Teste
 {
     public class Teste
     {
         [Fact]
-        public void Test1()
+        public void ZeroNaoENarcisista()
         {
+            //Arrange
+            var Valor = 22;
+            var resposta = 0;
 
+            //Act
+            resposta = Calculos.NumeroDeCiclos(Valor);
 
+            //Assert
+            resposta.Should().Be(16);
         }
     }
 }
