85548e42204a5fde3962404d7ec3e1b3559bfdcd
Thales Cancado Costa <Thales.Costa@rhimagnesita.com>
2023-09-01 08:46:14 -0300

    Desenvolvimento da aplicação Console e criação de um metodo para fazer o calculo de saida

---
+++ Thalesgalo/Biblioteca/Biblioteca/Calculos.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca
+{
+    public class Calculos
+    {
+        public static int NumeroDeCiclos(int n)
+        {
+            int cont = 0;
+
+            while (n != 1)
+            {
+                if (n % 2 == 0)
+                {
+                    n /= 2;
+                }
+                else
+                {
+                    n = 3 * n + 1;
+                }
+                cont++;
+            }
+
+            return cont + 1;
+        }
+        public static int CalculoDeSaida(int start, int end)
+        {
+            var max = 0;
+
+            for (int j = start; j <= end; j++)
+            {
+                var resultado = Calculos.NumeroDeCiclos(j);
+                if (resultado > max)
+                {
+                    max = resultado;
+                }
+            }
+            return max;
+        }
+    }
+}
--- Thalesgalo/Console/Program.cs
+++ Thalesgalo/Console/Program.cs
@@ -16,14 +16,41 @@ namespace ConsoleApp
 
             var RF = (Calculos.NumeroDeCiclos(n));
 
-            Console.WriteLine($"O numero de ciclos que o nº: {n} faz é {RF}");
-
             string EntradaDeDados = @"D:\GitHubRepos\3nPlus1\Thalesgalo\Leitura.txt";
             string SaidaDeDados = @"D:\GitHubRepos\3nPlus1\Thalesgalo\Escrita.txt";
 
-            var j = (Calculos.CalculoDeSaida(EntradaDeDados, SaidaDeDados));
+            try
+            {
+                //Leitura de todas as linhas;
+                string[] lines = File.ReadAllLines(EntradaDeDados);
 
-            // System.Diagnostics.Process.Start("notepad.exe", SaidaDeDados);
+                using (StreamWriter sw = new StreamWriter(SaidaDeDados))
+                {
+                    foreach (var i in lines)
+                    {
+                        string[] parts = i.Split(' ');
+
+                        var start = Convert.ToInt32(parts[0]);
+                        var end = Convert.ToInt32(parts[1]);
+
+                        sw.Write(start);
+                        sw.Write(' ');
+                        sw.Write(end);
+                        sw.Write(' ');
+                        sw.Write(Calculos.CalculoDeSaida(start, end));
+
+                        sw.WriteLine();
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("ERRO");
+            }
+
+            Console.WriteLine($"O numero de ciclos que o nº: {n} faz é {RF}");
+
+            System.Diagnostics.Process.Start("notepad.exe", SaidaDeDados);
 
             Console.ReadKey();
         }
