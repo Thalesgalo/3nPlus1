201e2862415fcc301367e0ff152b9febfd9c0175
Thales Cancado Costa <Thales.Costa@rhimagnesita.com>
2023-09-01 08:48:18 -0300

    Criação e desenvolvimento de testes com o objetivo de 100% de cobertura

--- Thalesgalo/Teste/Teste/UnitTest1.cs
+++ Thalesgalo/Teste/Teste/UnitTest1.cs
@@ -1,13 +1,14 @@
 using Biblioteca;
 using Xunit;
 using FluentAssertions;
+using Microsoft.VisualBasic;
 
 namespace Teste
 {
     public class Teste
     {
         [Fact]
-        public void ZeroNaoENarcisista()
+        public void VinteEDois()
         {
             //Arrange
             var Valor = 22;
@@ -19,5 +20,33 @@ namespace Teste
             //Assert
             resposta.Should().Be(16);
         }
+        
+        [Theory]
+        [InlineData(1, 10, 20)]
+        [InlineData(100, 200, 125)]
+        [InlineData(201, 210, 89)]
+        [InlineData(900, 1000, 174)]
+        public void VerificaNumerosDeSaida(int start, int end, int respostaEsperada)
+        {
+            //Act
+            var resposta = Calculos.CalculoDeSaida(start, end);
+
+            //Assert
+            resposta.Should().Be(respostaEsperada);
+        }
+        public void zero()
+        {
+            //Arrange
+            var Valor = 1;
+            var resposta = 0;
+
+            //Act
+            resposta = Calculos.NumeroDeCiclos(Valor);
+
+            //Assert
+            resposta.Should().Be(1);
+        }
     }
+
+
 }
