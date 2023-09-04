e1d95a152f3ab450cda63230735e3ff54f1209ff
Thales Cancado Costa <Thales.Costa@rhimagnesita.com>
2023-09-04 13:13:53 -0300

    Criação de testes para cobertura maior dos testes de mutação

--- Thalesgalo/Teste/Teste/Teste.cs
+++ Thalesgalo/Teste/Teste/Teste.cs
@@ -2,6 +2,8 @@ using Biblioteca;
 using Xunit;
 using FluentAssertions;
 using Microsoft.VisualBasic;
+using System.Reflection.Metadata.Ecma335;
+using Xunit.Sdk;
 
 namespace Teste
 {
@@ -20,9 +22,10 @@ namespace Teste
             //Assert
             resposta.Should().Be(16);
         }
-        
+     
         [Theory]
         [InlineData(1, 10, 20)]
+        [InlineData(100, 171, 125)]
         [InlineData(100, 200, 125)]
         [InlineData(201, 210, 89)]
         [InlineData(900, 1000, 174)]
@@ -34,18 +37,24 @@ namespace Teste
             //Assert
             resposta.Should().Be(respostaEsperada);
         }
-        public void zero()
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-22)]
+        public void VerificaNumeroZeroENegativo(int n)
         {
             //Arrange
-            var Valor = 1;
             var resposta = 0;
 
-            //Act
-            resposta = Calculos.NumeroDeCiclos(Valor);
+            //Acts
+            Action act = () => resposta = Calculos.NumeroDeCiclos(n);
 
             //Assert
-            resposta.Should().Be(1);
+            act.Should()
+              .Throw<ArgumentException>()
+              .WithMessage("Não pode ser numeros negativos");
         }
+        
     }
 
 
