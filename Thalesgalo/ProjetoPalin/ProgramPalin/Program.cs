8f1cdc125d0f85ceadaeb7d3bed896a4296e3031
Thales Cancado Costa <Thales.Costa@rhimagnesita.com>
2023-09-05 12:49:52 -0300

    Configuração do Projeto para link entre biblioteca e appconsole

---
+++ Thalesgalo/ProjetoPalin/ProgramPalin/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProgramPalin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
